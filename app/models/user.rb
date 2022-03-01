fadf07c14ec0aac1afdf5c1437d21f49a76abe01
cristian.ramirez <cristian.ramirez@enroutesystems.com>
2022-03-01 14:03:49 -0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-  
+  VALID_EMAIL_REGEX = /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/i
+
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: {maximum: 105},
+            format: { with: VALID_EMAIL_REGEX }
 end
