630710b5f9d9e3feef3fdaeefd0895f46f9513e8
cristian.ramirez <cristian.ramirez@enroutesystems.com>
2022-03-03 12:32:20 -0600

    Added form to users and made the errors view generic

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles, only: [:show, :index, :create, :destroy, :edit, :new, :update]
   get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  #resources :users, except: [:new]
 end
