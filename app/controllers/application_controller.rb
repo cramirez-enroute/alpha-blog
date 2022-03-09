21a76167d4ccce56afa72fa140c8aca3ab955206
cristian.ramirez <cristian.ramirez@enroutesystems.com>
2022-03-09 17:32:15 -0600

    Added pagination and pagination styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   UNPROCESSABLE_ENTITY_STATUS = 422
   OK_STATUS = 200
 
-  ARTICLES_PAGINATION_LIMIT = 10
+  ARTICLES_PAGINATION_LIMIT = 5
 
   def hello
     render html: 'Hello world'
