a1feaf203f509c43214135032a2e30c5a0237abe
cristian.ramirez <cristian.ramirez@enroutesystems.com>
2022-03-10 13:11:28 -0600

    Changed auth methods to application controller and made it available for both views and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,15 @@ class ApplicationController < ActionController::Base
   ARTICLES_PAGINATION_LIMIT = 5
   USERS_PAGINATION_LIMIT = 5
 
-  def hello
-    render html: 'Hello world'
+  ## This makes current_user available also for the views
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
