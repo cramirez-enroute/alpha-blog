dce6af339ac5f5ad6d55f74df29cfdac700c1a15
cristian.ramirez <cristian.ramirez@enroutesystems.com>
2022-03-10 13:14:58 -0600

    Updated controllers. Having access to authentication methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
